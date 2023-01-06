c2dbb7daa4c8ad0a90fe9d3d2e8fc6fd5586e4d9
abbas <dashatanad@gmail.com>
2023-01-06 23:48:24 +0330

    ok

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require("express") 
+const cors = require("cors")
+const app = express()
+app.listen(5000, () => console.log(`server is listening to port 5000`))
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.send('hello world, this is little node')
+})
+
